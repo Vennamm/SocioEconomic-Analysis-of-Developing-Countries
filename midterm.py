c52a88a12d54571cbc8df96ede914d2cf5a7bcd7
Vennamm <33382705+Vennamm@users.noreply.github.com>
2024-10-19 23:35:08 -0400

    Update midterm.py

--- midterm.py
+++ midterm.py
@@ -345,8 +345,8 @@ elif view == "Sandbox Mode":
         # ret_val = experiment.to_streamlit(ret="selected_uids", key="hip").display()
 
         # st.markdown("hiplot returned " + json.dumps(ret_val))
-        st.display(experiments)
-        # hip_exp = hip.Experiment.display_st(experiment)
+        
+        hip_exp = hip.Experiment.display_st(experiment)
 
 elif view == "Documentation":
     st.subheader("Documentation")
